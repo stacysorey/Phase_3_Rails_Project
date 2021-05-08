b462c0b37e7e5ad8f26aa149fa5889a8fcd59003
stacysorey <stacysorey@gmail.com>
2021-05-08 18:02:04 -0400

    edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Journal.create([
-  { title: "Journal 1"},
-  { title: "Journal 2"},
-  { title: "Journal 3"},
+  { id: 1, title: "Journal 1", user_id: 1, created_at: DateTime.now},
+  { id: 2, title: "Journal 2", user_id: 1, created_at: DateTime.now},
+  { id: 3, title: "Journal 3", user_id: 1, created_at: DateTime.now},
   ])
 
-Entry.create([
-  { title: "Journal Entry 1", category: "Daily Log", description: "Test description", user_id: 1, date: Date.today},
-])
