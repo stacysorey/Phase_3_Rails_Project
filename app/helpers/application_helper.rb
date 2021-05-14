dfa3c607dbcea2d8de64fa815d07500d5632ca76
stacysorey <stacysorey@gmail.com>
2021-05-14 06:27:24 -0400

    editing views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
     !!current_user
   end
 
+  def format_created_at(obj)
+    date = obj.created_at.to_date
+    date.strftime("%m.%d.%Y")
+  end
+
+  def format_date(obj)
+    date = obj.date
+    date.strftime("%m.%d.%Y")
+  end
+
 end
